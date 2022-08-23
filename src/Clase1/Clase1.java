2fb38f2d4fdb0bc6f068b883efd196980599f7a3
nscando <nicolas.scandizzo@gmail.com>
2022-08-22 21:02:04 -0300

    Inicializar cero

--- src/Clase1/Clase1.java
+++ src/Clase1/Clase1.java
@@ -24,6 +24,28 @@ public class Clase1 {
       }
    }
 
+
+   public void inicializarEnCero(int valorInicial) {
+      Nodo recorrido = raiz;
+      Nodo aux = new Nodo();
+      aux.info = recorrido.info;
+      System.out.println("Listado de todos los nodos inicializados en cero");
+
+      while (recorrido != null) {
+         recorrido = recorrido.sig;
+
+         Nodo temporal = new Nodo();
+
+         temporal.info = valorInicial;
+
+         aux.info = temporal.info;
+
+         System.out.println(aux.info + " - ");
+      }
+      System.out.println("--");
+   }
+
+
    public void imprimir() {
       Nodo flecha = raiz;
       System.out.println("Listado de todos los elementos de la pila");
@@ -35,12 +57,33 @@ public class Clase1 {
       System.out.println("--");
    }
 
+   public void cantidad() {
+      Nodo reco = raiz;
+      int contador = 0;
+
+      while (reco != null) {
+         reco = reco.sig;
+         contador++;
+      }
+      if (contador != 0) {
+         System.out.println("Esta Pila contiene: " + contador + " Nodos");
+      } else if (contador == 0) {
+         System.out.println("Esta pila esta vacia");
+      }
+   }
+
+
    public static void main(String[] args) {
       Clase1 pila1 = new Clase1();
+      pila1.insertar(5);
+      pila1.insertar(8);
       pila1.insertar(10);
-      pila1.insertar(90);
-      pila1.insertar(60);
-      pila1.insertar(15);
+      pila1.insertar(20);
+      pila1.insertar(20);
+      pila1.insertar(20);
+      pila1.insertar(20);
+      pila1.cantidad();
       pila1.imprimir();
+      pila1.inicializarEnCero(0);
    }
 }
