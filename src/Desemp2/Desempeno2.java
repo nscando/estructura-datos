0f3ca8a975e5ba4973dd340167f4f5f37814e9cb
nscando <nicolas.scandizzo@gmail.com>
2022-10-24 17:33:34 -0300

    Arboles Binarios

--- src/Desemp2/Desempeno2.java
+++ src/Desemp2/Desempeno2.java
@@ -63,11 +63,66 @@ public class Desempeno2 {
                System.out.println(x + " por DERECHA del NODO " + anterior.info);
             }
          }
+      } else {
+         System.out.println("El valor "+ x + " que intentas insertar ya existe");
       }
    }
 
 
+   public void menorValor() {
+      if (raiz == null) {
+         System.out.println("El Subarbol IZQUIERDO esta vacio");
+      } else {
 
+         Nodo reco = raiz;
+         Nodo auxiliar = raiz;
+         while (reco.izq != null) {
+            reco = reco.izq;
+            System.out.println("El MENOR valor del SUBARBOL IZQUIERDO es: " + reco.info);
+            auxiliar = reco.der;
+            System.out.println("El MAYOR valor del SUBARBOL IZQUIERO es: " + auxiliar.info);
+
+         }
+      }
+   }
+
+   private void retornarAltura(Nodo reco, int nivel) {
+      if (reco != null) {
+         retornarAltura(reco.izq, nivel + 1);
+         if (nivel > altura) {
+            altura = nivel;
+         }
+         retornarAltura(reco.der, nivel + 1);
+      }
+   }
+
+
+   public int retornarAltura() {
+      altura = 0;
+      retornarAltura(raiz, 1);
+      return altura;
+   }
+
+   public boolean existePrimerosTresNiveles(int info) {
+      Nodo reco = raiz;
+
+      while (reco != null && retornarAltura() == 3) {
+         if (info == reco.info) {
+            System.out.println("El valor " + info + " existe en los primeros 3 niveles!");
+            return true;
+
+         } else {
+            if (info > reco.info) {
+               reco = reco.der;
+            } else {
+               reco = reco.izq;
+            }
+         }
+      }
+      System.out.println("El valor " + info + " no existe en los 3 primeros niveles!");
+
+      return false;
+   }
 
    //Visita la raiz
    //Recorre subarbol izq en pre-orden
@@ -126,6 +181,16 @@ public class Desempeno2 {
       arbol.insertar(9);
       arbol.insertar(20);
       arbol.insertar(15);
+      arbol.insertar(15);
+
+      System.out.println();
+      arbol.menorValor();
+      System.out.println();
+      System.out.println("Altura del arbol: ");
+      System.out.println(arbol.retornarAltura());
+      System.out.println();
+      System.out.println("Existe:");
+      arbol.existePrimerosTresNiveles(15);
       System.out.println();
       arbol.recorrerEntreOrden();
       arbol.recorrerPreOrden();
